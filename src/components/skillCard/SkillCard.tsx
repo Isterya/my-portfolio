43539fa995df320401c1faed924265b69faa6e38
isterya <vash.vybor.1@gmail.com>
2025-04-02 21:56:05 +0200

    feat: add SkillCard component

--- src/components/skillCard/SkillCard.tsx
+++ src/components/skillCard/SkillCard.tsx
@@ -1,5 +1,28 @@
-const SkillCard = () => {
-  return <></>;
+import './skillCard.scss';
+
+interface SkillCardProps {
+  title: string;
+  icon: string;
+  description: string;
+}
+
+const SkillCard: React.FC<SkillCardProps> = ({ title, icon, description }) => {
+  return (
+    <div className="skill-card">
+      <div className="skill-card__header">
+        <h3>{title}</h3>
+      </div>
+      <div className="skill-card__divider"></div>
+
+      <div className="skill-card__wrapper">
+        <div className="skill-card__bg skill-card__bg--first"></div>
+        <div className="skill-card__bg skill-card__bg--second"></div>
+        <div className="skill-card__bg skill-card__bg--third">
+          <p>{description}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SkillCard;
