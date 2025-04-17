cde596182f326ce0c79c120061bb06db5f7e8892
isterya <vash.vybor.1@gmail.com>
2025-04-17 22:16:10 +0200

    feat: add, style and fully implement Page 404

--- src/pages/page404/Page404.tsx
+++ src/pages/page404/Page404.tsx
@@ -1,9 +1,34 @@
+import { Link } from 'react-router-dom';
+
+import errorImg from '../../assets/img/404-error.svg';
+
 import './page404.scss';
 
 const Page404 = () => {
   return (
-    <div>
-      <h1>Error 404! Something went wrong</h1>
+    <div className="page404">
+      <div className="container">
+        <div className="page404__bg page404__bg--top"></div>
+        <div className="page404__bg page404__bg--bottom"></div>
+
+        <div className="page404__content">
+          <div className="page404__text">
+            <h1>Oops! Looks like this page doesn't exist.</h1>
+            <Link
+              to="/"
+              aria-label="Return to home page"
+            >
+              BACK TO HOME
+            </Link>
+          </div>
+
+          <img
+            className="page404__content-img"
+            src={errorImg}
+            alt="404 error img"
+          />
+        </div>
+      </div>
     </div>
   );
 };
