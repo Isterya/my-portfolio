1754e8fb082c10ae4d5be81ef4492757fa6c6076
isterya <vash.vybor.1@gmail.com>
2025-04-26 13:11:23 +0200

    feat: translate TableOfContents component into other languages

--- src/components/tableOfContents/TableOfContents.tsx
+++ src/components/tableOfContents/TableOfContents.tsx
@@ -1,3 +1,5 @@
+import { useTranslation } from 'react-i18next';
+
 import './tableOfContents.scss';
 
 type Section = {
@@ -10,16 +12,15 @@ type TableOfContentsProps = {
 };
 
 const TableOfContents = ({ sections }: TableOfContentsProps) => {
+  const { t } = useTranslation();
+
   return (
-    <nav
-      className="toc"
-      aria-label="Table of contents"
-    >
-      <h4>On this page</h4>
+    <nav className="toc" aria-label="Table of contents">
+      <h4>{t('tableOfContents.title')}</h4>
       <ul>
         {sections.map((section) => (
           <li key={section.id}>
-            <a href={`#${section.id}`}>{section.title}</a>
+            <a href={`#${section.id}`}>{t(section.title)}</a>
           </li>
         ))}
       </ul>
