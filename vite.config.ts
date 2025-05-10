c8a0eef2a461093c147db7960538b3ecc352fc2a
isterya <vash.vybor.1@gmail.com>
2025-05-10 12:12:41 +0200

    chore: bring the project and configs to perfection, prepare for a build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import path from 'path';
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -16,7 +15,6 @@ export default defineConfig({
       },
     },
   },
-
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
